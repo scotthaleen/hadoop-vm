b99855dbfcf4b59913689662fb8654ebd5e6bd39
Scott Haleen <scott.haleen@gmail.com>
2015-01-20 12:46:03 -0500

    added javahome to profile

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@
 ## repos
 . /vagrant/provision/java-repo.sh
 
-mv /vagrant/artifacts/cloudera.list /etc/apt/sources.list.d/cloudera.list
+cp /vagrant/artifacts/cloudera.list /etc/apt/sources.list.d/cloudera.list
 curl -s http://archive.cloudera.com/cdh5/ubuntu/trusty/amd64/cdh/archive.key | sudo apt-key add -
 
 apt-get -y update
--- provision/java.sh
+++ provision/java.sh
@@ -2,3 +2,5 @@
 
 printf "Install \e[0;36mJava JDK 7\e[0m \n"
 apt-get -y install oracle-java7-installer 
+
+cp /vagrant/artifacts/java.sh /etc/profile.d/java.sh
