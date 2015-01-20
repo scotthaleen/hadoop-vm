906f98a5b76275aaf0a5bc48a9e3b5b35f80f23b
Scott Haleen <scott.haleen@gmail.com>
2015-01-20 13:52:53 -0500

    fixed spark provision

--- provision/spark.sh
+++ provision/spark.sh
@@ -9,7 +9,7 @@ tar -zxvf spark-1.2.0-bin-hadoop2.3.tgz
 mv spark-1.2.0-bin-hadoop2.3 /srv/software/
 
 chown vagrant:vagrant -R /srv/software/spark-1.2.0-bin-hadoop2.3
-sudo ln -s spark-1.2.0-bin-hadoop2.3/ /opt/spark
+sudo ln -s /srv/software/spark-1.2.0-bin-hadoop2.3/ /opt/spark
 
 mkdir -p /opt/spark/logs
 chmod 777 /opt/spark/logs
